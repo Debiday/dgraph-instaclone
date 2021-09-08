3f45c51044402e1d28459fcf792df17f9427f353
Deborah Ong <deborah.oyt@gmail.com>
2021-09-08 15:38:34 -0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
 }
+
+const App = () => (
+  <Grommet theme={theme} full>
+    <Box fill background="light-3">
+      <NavBar />
+    </Box>
+  </Grommet>
+)
 
 export default App;
